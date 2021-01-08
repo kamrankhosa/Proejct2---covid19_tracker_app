62009264d30a07d6617c51d45e67bc60e4c287e2
kamrankhosa <37815528+kamrankhosa@users.noreply.github.com>
2021-01-08 21:29:45 +0500

    Header added to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="container">
-      <h1>Covid<sup>19</sup> Tracker App</h1>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <h1>Covid<sup>19</sup> Tracker App</h1>
+  )
+}
+
+export default Header
